08f387042f6612362cbf15bbcc7888c77b9356fb
Hữu Dũng <meokg456@gmail.com>
2021-09-28 11:14:48 +0700

    + add 2 cases update todo status reducer: false and null

--- test/todo_reducer_test.dart
+++ test/todo_reducer_test.dart
@@ -18,7 +18,25 @@ void main() {
       };
     });
     test("Update todo status to true", () {
+      final TodosModel modifiedModel = updateTodoStatusReducer!(id: 2, isComplete: true);
+      expect(
+          modifiedModel,
+          firstState!.todosModel.rebuild((model) => model
+            ..todos.updateValue(2, (todo) => todo.rebuild((todo) => todo.isCompleted = true))
+            ..incompleteTodos.remove(2)
+            ..completedTodos.add(2)));
+    });
+    test("Update todo status to false", () {
       final TodosModel modifiedModel = updateTodoStatusReducer!(id: 1, isComplete: false);
+      expect(
+          modifiedModel,
+          firstState!.todosModel.rebuild((model) => model
+            ..todos.updateValue(1, (todo) => todo.rebuild((todo) => todo.isCompleted = false))
+            ..incompleteTodos.add(1)
+            ..completedTodos.remove(1)));
+    });
+    test("Update todo status to null", () {
+      final TodosModel modifiedModel = updateTodoStatusReducer!(id: 1, isComplete: null);
       expect(
           modifiedModel,
           firstState!.todosModel.rebuild((model) => model
