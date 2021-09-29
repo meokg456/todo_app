bd9fd17135156e8b41b3f6a9ddf148b13473a5fd
Hữu Dũng <meokg456@gmail.com>
2021-09-29 09:00:00 +0700

    Adjust integration test

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -15,69 +15,57 @@ void main() {
 
     test("Update todo status", () async {
       var checkbox1 = find.byValueKey("checkbox1");
-      await driver.waitFor(checkbox1);
       await driver.tap(checkbox1);
+      await driver.waitFor(checkbox1);
 
       var checkbox2 = find.byValueKey("checkbox2");
-      await driver.waitFor(checkbox2);
       await driver.tap(checkbox2);
+      await driver.waitFor(checkbox2);
 
       var checkbox5 = find.byValueKey("checkbox5");
-      await driver.waitFor(checkbox5);
       await driver.tap(checkbox5);
+      await driver.waitFor(checkbox5);
 
       var incomplete = find.byValueKey("incomplete");
-      await driver.waitFor(incomplete);
       await driver.tap(incomplete);
+      await driver.waitFor(incomplete);
 
       checkbox1 = find.byValueKey("checkbox1");
-      await driver.waitFor(checkbox1);
       await driver.tap(checkbox1);
 
-      await Future.delayed(Duration(milliseconds: 500));
-
       var checkbox6 = find.byValueKey("checkbox6");
-      await driver.waitFor(checkbox6);
       await driver.tap(checkbox6);
-
-      await Future.delayed(Duration(milliseconds: 500));
 
       var completed = find.byValueKey("completed");
-      await driver.waitFor(completed);
       await driver.tap(completed);
+      await driver.waitFor(completed);
 
       checkbox6 = find.byValueKey("checkbox6");
-      await driver.waitFor(checkbox6);
       await driver.tap(checkbox6);
 
-      await Future.delayed(Duration(milliseconds: 500));
-
       checkbox5 = find.byValueKey("checkbox5");
-      await driver.waitFor(checkbox5);
       await driver.tap(checkbox5);
 
-      await Future.delayed(Duration(milliseconds: 500));
-
       var todos = find.byValueKey("todos");
-      await driver.waitFor(todos);
       await driver.tap(todos);
+      await driver.waitFor(todos);
 
       await Future.delayed(Duration(seconds: 2));
     });
 
     test("Create todo", () async {
       var fab = find.byValueKey("create");
-
-      await driver.waitFor(fab);
       await driver.tap(fab);
+      await driver.waitFor(fab);
 
       var note = find.byValueKey("note");
-      await driver.waitFor(note);
       var done = find.byValueKey("done");
       await driver.tap(done);
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(note);
+      await driver.waitFor(done);
+
       await driver.enterText("Sleep");
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(note);
 
       await driver.tap(done);
 
@@ -86,65 +74,48 @@ void main() {
 
     test("Edit todo", () async {
       var todo = find.byValueKey("todo1");
-      await driver.waitFor(todo);
       await driver.tap(todo);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(todo);
 
       var edit = find.byValueKey("edit");
-      await driver.waitFor(edit);
       await driver.tap(edit);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(edit);
 
       var note = find.byValueKey("note");
-      await driver.waitFor(note);
       await driver.enterText("");
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(note);
 
       var done = find.byValueKey("done");
       await driver.tap(done);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(done);
 
       await driver.enterText("Sleep");
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(note);
+
       await driver.tap(done);
       await Future.delayed(Duration(milliseconds: 1000));
     });
 
     test("Delete todo", () async {
       var todo = find.byValueKey("todo2");
-      await driver.waitFor(todo);
       await driver.tap(todo);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(todo);
 
       var delete = find.byValueKey("delete");
-      await driver.waitFor(delete);
       await driver.tap(delete);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(delete);
 
       var no = find.byValueKey("no");
-      await driver.waitFor(no);
       await driver.tap(no);
 
-      await Future.delayed(Duration(milliseconds: 500));
-
       todo = find.byValueKey("todo5");
-      await driver.waitFor(todo);
       await driver.tap(todo);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(todo);
 
       await driver.tap(delete);
-
-      await Future.delayed(Duration(milliseconds: 500));
+      await driver.waitFor(delete);
 
       var yes = find.byValueKey("yes");
-      await driver.waitFor(yes);
       await driver.tap(yes);
 
       await Future.delayed(Duration(milliseconds: 2000));
